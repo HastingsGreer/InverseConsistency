d69f13cc68f4fd95c831c87f1e71771374db2ecb
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-01-16 11:13:30 -0500

    pair up sunnyside images

--- training_scripts/sunnyside.py
+++ training_scripts/sunnyside.py
@@ -2,7 +2,7 @@ import parent
 import data
 import networks
 import visualize
-import train
+#import train
 import inverseConsistentNet
 import numpy as np
 import torch
@@ -12,11 +12,44 @@ import os
 
 import describe
 
+def train2d(net, optimizer, d1, epochs=400):
 
-d1, d2 = data.get_dataset_sunnyside("train", scale=.7
+    loss_history = []
+    print("[", end="")
+    for epoch in range(epochs):
+        print("-", end="", flush=True)
+        if (epoch + 1) % 50 == 0:
+            print("]", end="\n[")
+        for A in d1:
+            if A[0].size()[0] == 128:
+                image_A = A[0].cuda()
+                image_B = A[1].cuda()
+                optimizer.zero_grad()
+                loss, inverse_consistency_loss, similarity_loss, transform_magnitude = net(
+                    image_A, image_B
+                )
+
+                loss.backward()
+                optimizer.step()
+        du = (net.phi_AB[:, :, 1:, :-1] - net.phi_AB[:, :, :-1, :-1]).detach().cpu()
+        dv = (net.phi_AB[:, :, :-1, 1:] - net.phi_AB[:, :, :-1, :-1]).detach().cpu()
+        dA = du[:, 0] * dv[:, 1] - du[:, 1] * dv[:, 0]
+
+        loss_history.append(
+            [
+                inverse_consistency_loss.item(),
+                similarity_loss.item(),
+                transform_magnitude.item(),
+                torch.log(torch.sum(dA < 0) + 0.1),
+            ]
+        )
+    print("]")
+    return loss_history
+
+d1 = data.get_dataset_sunnyside("train", scale=.7
 )
 
-d1_t, d2_t = data.get_dataset_sunnyside("test", scale=.7
+d1_t = data.get_dataset_sunnyside("test", scale=.7
 )
 network = networks.tallUNet2
 
@@ -34,8 +67,8 @@ net.cuda()
 optimizer = torch.optim.Adam(net.parameters(), lr=0.001)
 net.train()
 xs = []
-for _ in range(40):
-    y = np.array(train.train2d(net, optimizer, d1, d2, epochs=5))
+for _ in range(240):
+    y = np.array(train2d(net, optimizer, d1, epochs=5))
     xs.append(y)
     x = np.concatenate(xs)
     plt.title(
@@ -53,14 +86,14 @@ for _ in range(40):
     # torch.manual_seed(1)
     # torch.cuda.manual_seed(1)
     # np.random.seed(1)
-    image_A, image_B = (x[0].cuda() for x in next(zip(d1_t, d2_t)))
+    image_A, image_B = [im.cuda() for im in next(iter(d1_t))]
     for N in range(3):
         visualize.visualizeRegistration(
             net,
             image_A,
             image_B,
             N,
-            describe.run_dir + "epoch" + str(_) + "case" + str(N) + ".png",
+            describe.run_dir + f"epoch{_:03}" + "case" + str(N) + ".png",
         )
 random.seed(1)
 torch.manual_seed(1)
