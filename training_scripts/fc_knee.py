ebd316ea58f2c9a65c2cd9b7c36a511f2ae72d6c
Thomas Greer <tgreer@biag-gpu0.cs.unc.edu>
2021-10-11 14:04:38 -0400

    fully connected 3-D registration experiment

--- training_scripts/fc_knee.py
+++ training_scripts/fc_knee.py
@@ -9,31 +9,38 @@ import network_wrappers
 import data
 import describe
 
-BATCH_SIZE = 8
+BATCH_SIZE = 32
 SCALE = 1  # 1 IS QUARTER RES, 2 IS HALF RES, 4 IS FULL RES
 input_shape = [BATCH_SIZE, 1, 40 * SCALE, 96 * SCALE, 96 * SCALE]
 
 GPUS = 1
 
-
-
+scale_down = [BATCH_SIZE, 1, 20 * SCALE, 48 * SCALE, 48 * SCALE]
 net = inverseConsistentNet.InverseConsistentNet(
-    network_wrappers.FunctionFromVectorField(networks.FCNet3D(input_shape)),
+    network_wrappers.DownsampleNet(
+        network_wrappers.FunctionFromVectorField(networks.FCNet3D(scale_down, bottleneck=512)),
+        dimension=3,
+    ),
     lambda x, y: torch.mean((x - y) ** 2),
-    400,
+    70,
 )
 
 network_wrappers.assignIdentityMap(net, input_shape)
 
 
 knees = torch.load("/playpen/tgreer/knees_big_train_set")
+trained_weights = torch.load("results/fc_smol_knee/knee_aligner_resi_net30000")
+network_wrappers.adjust_batch_size(net, 16)
+net.load_state_dict(trained_weights)
+network_wrappers.adjust_batch_size(net, BATCH_SIZE)
 
 if GPUS == 1:
     net_par = net.cuda()
 else:
     net_par = torch.nn.DataParallel(net).cuda()
 optimizer = torch.optim.Adam(net_par.parameters(), lr=0.00001)
-
+optimizer_state = torch.load("results/fc_smol_knee/knee_aligner_resi_opt30000")
+optimizer.load_state_dict(optimizer_state)
 
 net_par.train()
 
