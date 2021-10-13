8e83b313e4a9e45841287d2bce165be5248ec111
HastingsGreer <hastings.greer@gmail.com>
2021-10-13 17:47:19 -0400

    Fix bug in triangles demo caused by merging in advanced similarity metric

--- training_scripts/2d_triangles_example.py
+++ training_scripts/2d_triangles_example.py
@@ -38,7 +38,7 @@ net = inverseConsistentNet.InverseConsistentNet(
     network_wrappers.FunctionFromVectorField(networks.tallUNet2(dimension=2)),
     # Our image similarity metric. The last channel of x and y is whether the value is interpolated or extrapolated, 
     # which is used by some metrics but not this one
-    lambda x, y: torch.mean((x[:, 1:] - y[:, 1:]) ** 2),
+    lambda x, y: torch.mean((x[:, :1] - y[:, :1]) ** 2),
     lmbda,
 )
 
