9f8f02aed3c5abb44d4d124c38d0ed73a0e24921
HastingsGreer <hastings.greer@gmail.com>
2022-01-04 00:40:25 -0500

    Delete parent.py

--- training_scripts/parent.py
+++
@@ -1,7 +0,0 @@
-# Python sins, I sin. In the end all is forgiven?
-from inspect import getsourcefile
-import os.path as path, sys
-print("WARNING: I can't really recommend using .")
-
-current_dir = path.dirname(path.abspath(getsourcefile(lambda: 0)))
-sys.path.insert(0, current_dir[: current_dir.rfind(path.sep)] + "/src/icon_registration")
