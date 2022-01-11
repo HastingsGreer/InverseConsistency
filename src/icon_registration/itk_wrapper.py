52c5ca140c4cbe340be893bbe44a0e09b2aa7bb1
Hastings Greer <hastings@hgreer.com>
2022-01-10 19:02:06 -0500

    workin on that itk registration

--- src/icon_registration/itk_wrapper.py
+++ src/icon_registration/itk_wrapper.py
@@ -1,4 +1,5 @@
 import itk
+import matplotlib.pyplot as plt
 import numpy as np
 import torch
 import torch.nn.functional as F
@@ -6,7 +7,14 @@ import torch.nn.functional as F
 import icon_registration.pretrained_models
 import icon_registration.network_wrappers
 
+import icon_registration.visualize
+
 def register_pair(model, image_A, image_B):
+
+
+    outdir = "/home/hastings/blog/_assets/ICON_test/"
+    import subprocess
+    subprocess.run("rm -r " + outdir + "*", shell=True)
     assert( isinstance(image_A, itk.Image))
     assert( isinstance(image_B, itk.Image))
     icon_registration.network_wrappers.adjust_batch_size(model, 1)
@@ -21,17 +29,81 @@ def register_pair(model, image_A, image_B):
 
     print("A shape:", A_trch.shape)
     print("B shape:", B_trch.shape)
+    
 
 
     A_resized = F.interpolate(A_trch, size=shape[2:], mode="trilinear", align_corners=False)
     B_resized = F.interpolate(B_trch, size=shape[2:], mode="trilinear", align_corners=False)
+
     
     print("A shape:", A_resized.shape)
     print("B shape:", B_resized.shape)
+    print("A_max", torch.max(A_resized))
+
+    plt.imshow(A_resized[0, 0, 40].cpu())
+    plt.colorbar()
+    plt.savefig(outdir + "A_in.png")
+    plt.clf()
+
+    plt.imshow(B_resized[0, 0, 40].cpu())
+    plt.colorbar()
+    plt.savefig(outdir + "B_in.png")
+    plt.clf()
     with torch.no_grad():
         x = model(A_resized, B_resized)
+    
+    phi_AB = model.phi_AB(model.identityMap)[0].cpu()
+    disp_AB = phi_AB - model.identityMap[0].cpu()
+    disp_AB *= torch.Tensor([[[[80]]], [[[192]]], [[[192]]]])
+    icon_registration.visualize.show_as_grid(phi_AB[[1, 2], 40])
+    plt.savefig(outdir + "transform2.png")
+    plt.clf()
 
+    # Pass 1: try just using the itk utils to register not respecting spacing
+
+    fake_A = itk.image_from_array(A_resized.float().cpu()[0, 0])
+    fake_B = itk.image_from_array(B_resized.float().cpu()[0, 0])
+    
+    print(type(fake_A))
+    
+    
+    tr = itk.DisplacementFieldTransform[(itk.D, 3)].New()       
+
+    itk_disp_field = array_to_vector_image(disp_AB.long().numpy())
+
+    tr.SetDisplacementField(itk_disp_field)
+
+    interpolator = itk.LinearInterpolateImageFunction.New(fake_A)
+
+    warped_a = itk.resample_image_filter(fake_A, 
+        transform=tr, 
+        interpolator=interpolator,
+        size=itk.size(fake_A),
+        output_spacing=itk.spacing(fake_A)
+        )
+
+    warped_a_arr = np.array(warped_a)
+
+    plt.imshow(warped_a_arr[40] - A_resized[0, 0, 40].cpu().numpy())
+    plt.colorbar()
+    plt.savefig(outdir + "warpedA.png")
+    plt.clf()
     return None, None
 
+def array_to_vector_image(array):
+    # array is a numpy array of doubles of shape 
+    # 3, H, W, D
+
+    # returns an itk.Image of vectors
+    # returns image with [1, 1, 1] spacing :(
+    assert isinstance(array, np.ndarray)
 
 
+    PixelType = itk.Vector[itk.D, 3]
+    ImageType = itk.Image[PixelType, 3]
+
+    vector_image = itk.PyBuffer[ImageType].GetImageViewFromArray(array)
+
+    return vector_image
+    
+    
