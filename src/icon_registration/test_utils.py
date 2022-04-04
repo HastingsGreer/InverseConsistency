60d666c2a2aa4be5208b22523a0c0b938411cecd
HastingsGreer <hastings.greer@gmail.com>
2022-04-04 15:04:37 -0400

    Create windows-test-action.yml (#22)
    * Create windows-test-action.yml
    
    * Update windows-test-action.yml
    
    * Update test_utils.py

--- src/icon_registration/test_utils.py
+++ src/icon_registration/test_utils.py
@@ -7,4 +7,4 @@ TEST_DATA_DIR = pathlib.Path(__file__).parent.parent.parent / "test_files"
 
 
 def download_test_data():
-    subprocess.run(["girder-client",  "--api-url", "https://data.kitware.com/api/v1", "localsync", "61d3a99d4acac99f429277d7", TEST_DATA_DIR], stdout=sys.stdout)
+    subprocess.run(["girder-client",  "--api-url", "https://data.kitware.com/api/v1", "localsync", "61d3a99d4acac99f429277d7", str(TEST_DATA_DIR)], stdout=sys.stdout)
