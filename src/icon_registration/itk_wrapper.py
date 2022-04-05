6275cdd6dbb2f7329d6b69a56f66f8d40892603c
HastingsGreer <hastings.greer@gmail.com>
2022-04-05 09:57:07 -0400

    Update itk_wrapper.py (#24)
    * Update itk_wrapper.py
    
    * Update itk_wrapper.py
    
    * Update itk_wrapper.py

--- src/icon_registration/itk_wrapper.py
+++ src/icon_registration/itk_wrapper.py
@@ -7,14 +7,13 @@ import torch.nn.functional as F
 import icon_registration.pretrained_models
 import icon_registration.network_wrappers
 
-import icon_registration.visualize
-
 import icon_registration.config as config
 
 def register_pair(model, image_A, image_B):
 
     assert( isinstance(image_A, itk.Image))
     assert( isinstance(image_B, itk.Image))
+    
     icon_registration.network_wrappers.adjust_batch_size(model, 1)
     model.to(config.device) 
 
@@ -24,16 +23,22 @@ def register_pair(model, image_A, image_B):
     B_trch = torch.Tensor(B_npy).to(config.device)[None, None]
 
     shape = model.identityMap.shape
-
+    
+    # Here we resize the input images to the shape expected by the neural network. This affects the 
+    # pixel stride as well as the magnitude of the displacement vectors of the resulting displacement field, which
+    # create_itk_transform will have to compensate for.
     A_resized = F.interpolate(A_trch, size=shape[2:], mode="trilinear", align_corners=False)
     B_resized = F.interpolate(B_trch, size=shape[2:], mode="trilinear", align_corners=False)
 
     with torch.no_grad():
-        x = model(A_resized, B_resized)
+        model(A_resized, B_resized)
     
-    phi_AB = model.phi_AB(model.identityMap)[0].cpu()
-    phi_BA = model.phi_BA(model.identityMap)[0].cpu()
-
+    # phi_AB and phi_BA are [1, 3, H, W, D] pytorch tensors representing the forward and backward 
+    # maps computed by the model
+    phi_AB = model.phi_AB(model.identityMap)
+    phi_BA = model.phi_BA(model.identityMap)
+    
+    # the parameters ident, image_A, and image_B are used for their metadata
     return (
         create_itk_transform(phi_AB, model.identityMap, image_A, image_B),
         create_itk_transform(phi_BA, model.identityMap, image_B, image_A)
@@ -41,33 +46,40 @@ def register_pair(model, image_A, image_B):
 
 def create_itk_transform(phi, ident, image_A, image_B):
     
-    disp = phi - ident[0].cpu()
+    # itk.DeformationFieldTransform expects a displacement field, so we subtract off the identity map.
+    disp = (phi - ident)[0].cpu()
 
     network_shape_list = list(ident.shape[2:])
 
     dimension = len(network_shape_list)
+    
+    tr = itk.DisplacementFieldTransform[(itk.D, dimension)].New()       
 
+    # We convert the displacement field into an itk Vector Image. 
     scale = torch.Tensor(network_shape_list)
+   
     for _ in network_shape_list:
         scale = scale[:, None]
     disp *= scale
-    tr = itk.DisplacementFieldTransform[(itk.D, dimension)].New()       
-
+    
+    # disp is a shape [3, H, W, D] tensor with vector components in the order [vi, vj, vk]
     disp_itk_format  = disp.double().numpy()[list(reversed(range(dimension)))].transpose(list(range(1, dimension + 1)) + [0])
+    # disp_itk_format is a shape [H, W, D, 3] array with vector components in the order [vk, vj, vi]
+    # as expected by itk.
 
     itk_disp_field = itk.image_from_array(disp_itk_format, is_vector=True)
 
     tr.SetDisplacementField(itk_disp_field)
 
-    to_aligned = resampling_transform(image_A, list(reversed(network_shape_list)))
+    to_network_space = resampling_transform(image_A, list(reversed(network_shape_list)))
 
-    from_aligned = resampling_transform(image_B, list(reversed(network_shape_list))).GetInverseTransform()
+    from_network_space = resampling_transform(image_B, list(reversed(network_shape_list))).GetInverseTransform()
 
     phi_AB_itk = itk.CompositeTransform[itk.D, dimension].New()
 
-    phi_AB_itk.PrependTransform(from_aligned)
+    phi_AB_itk.PrependTransform(from_network_space)
     phi_AB_itk.PrependTransform(tr)
-    phi_AB_itk.PrependTransform(to_aligned)
+    phi_AB_itk.PrependTransform(to_network_space)
 
     return phi_AB_itk
 
