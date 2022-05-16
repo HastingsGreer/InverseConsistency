4acdee150047f942c4976e21bcb6f4b0deff1f37
HastingsGreer <hastings.greer@gmail.com>
2022-05-15 20:29:14 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("/Users/hastings/InverseConsistency/src/"))
+sys.path.insert(0, os.path.abspath("../../src/"))
 import icon_registration
 
 
