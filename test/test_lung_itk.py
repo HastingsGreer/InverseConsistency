fe8bed08e61d19d8401fe3a8e8891f507eb368a4
HastingsGreer <hastings.greer@gmail.com>
2022-06-10 14:22:30 -0400

    Update test_lung_itk.py

--- test/test_lung_itk.py
+++ test/test_lung_itk.py
@@ -98,10 +98,10 @@ class TestItkRegistration(unittest.TestCase):
         plt.savefig(footsteps.output_dir + "difference_lung.png")
         plt.clf()
 
-        insp_points = read_copd_pointset(
+        insp_points = icon_registration.test_utils.read_copd_pointset(
             "test_files/lung_test_data/copd1_300_iBH_xyz_r1.txt"
         )
-        exp_points = read_copd_pointset(
+        exp_points = icon_registration.test_utils.read_copd_pointset(
             "test_files/lung_test_data/copd1_300_eBH_xyz_r1.txt"
         )
         dists = []
@@ -124,44 +124,3 @@ class TestItkRegistration(unittest.TestCase):
         print(np.mean(dists))
         self.assertLess(np.mean(dists), 2.3)
 
-COPD_spacing = {
-    "copd1": [0.625, 0.625, 2.5],
-    "copd2": [0.645, 0.645, 2.5],
-    "copd3": [0.652, 0.652, 2.5],
-    "copd4": [0.590, 0.590, 2.5],
-    "copd5": [0.647, 0.647, 2.5],
-    "copd6": [0.633, 0.633, 2.5],
-    "copd7": [0.625, 0.625, 2.5],
-    "copd8": [0.586, 0.586, 2.5],
-    "copd9": [0.664, 0.664, 2.5],
-    "copd10": [0.742, 0.742, 2.5],
-}
-
-
-def read_copd_pointset(f_path):
-    """
-    :param f_path: the path to the file containing the position of points.
-    Points are deliminated by '\n' and X,Y,Z of each point are deliminated by '\t'.
-    :return: numpy array of points in physical coordinates
-    """
-    spacing = COPD_spacing[f_path.split("/")[-1].split("_")[0]]
-    spacing = np.expand_dims(spacing, 0)
-    with open(f_path) as fp:
-        content = fp.read().split("\n")
-
-        # Read number of points from second
-        count = len(content) - 1
-
-        # Read the points
-        points = np.ndarray([count, 3], dtype=np.float64)
-        for i in range(count):
-            if content[i] == "":
-                break
-            temp = content[i].split("\t")
-            points[i, 0] = float(temp[0])
-            points[i, 1] = float(temp[1])
-            points[i, 2] = float(temp[2])
-
-        # The copd gene points are in index space instead of physical space.
-        # Move them to physical space.
-        return (points - 1) * spacing
