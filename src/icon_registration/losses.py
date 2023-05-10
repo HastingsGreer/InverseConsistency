2ef171041415dea17cc187491d2b4024951473f0
HastingsGreer <hastings.greer@gmail.com>
2023-05-09 22:17:58 -0400

    Update losses.py
    This is the configuration we actually used in training

--- src/icon_registration/losses.py
+++ src/icon_registration/losses.py
@@ -235,6 +235,126 @@ class GradientICON(network_wrappers.RegistrationModule):
             flips(self.phi_BA_vectorfield),
         )
     
+
+class GradientICONSparse(network_wrappers.RegistrationModule):
+    def __init__(self, network, similarity, lmbda):
+
+        super().__init__()
+
+        self.regis_net = network
+        self.lmbda = lmbda
+        self.similarity = similarity
+
+    def forward(self, image_A, image_B):
+
+        assert self.identity_map.shape[2:] == image_A.shape[2:]
+        assert self.identity_map.shape[2:] == image_B.shape[2:]
+
+        # Tag used elsewhere for optimization.
+        # Must be set at beginning of forward b/c not preserved by .cuda() etc
+        self.identity_map.isIdentity = True
+
+        self.phi_AB = self.regis_net(image_A, image_B)
+        self.phi_BA = self.regis_net(image_B, image_A)
+
+        self.phi_AB_vectorfield = self.phi_AB(self.identity_map)
+        self.phi_BA_vectorfield = self.phi_BA(self.identity_map)
+
+        # tag images during warping so that the similarity measure
+        # can use information about whether a sample is interpolated
+        # or extrapolated
+
+        if getattr(self.similarity, "isInterpolated", False):
+            # tag images during warping so that the similarity measure
+            # can use information about whether a sample is interpolated
+            # or extrapolated
+            inbounds_tag = torch.zeros([image_A.shape[0]] + [1] + list(image_A.shape[2:]), device=image_A.device)
+            if len(self.input_shape) - 2 == 3:
+                inbounds_tag[:, :, 1:-1, 1:-1, 1:-1] = 1.0
+            elif len(self.input_shape) - 2 == 2:
+                inbounds_tag[:, :, 1:-1, 1:-1] = 1.0
+            else:
+                inbounds_tag[:, :, 1:-1] = 1.0
+        else:
+            inbounds_tag = None
+
+        self.warped_image_A = compute_warped_image_multiNC(
+            torch.cat([image_A, inbounds_tag], axis=1) if inbounds_tag is not None else image_A,
+            self.phi_AB_vectorfield,
+            self.spacing,
+            1,
+        )
+        self.warped_image_B = compute_warped_image_multiNC(
+            torch.cat([image_B, inbounds_tag], axis=1) if inbounds_tag is not None else image_B,
+            self.phi_BA_vectorfield,
+            self.spacing,
+            1,
+        )
+
+        similarity_loss = self.similarity(
+            self.warped_image_A, image_B
+        ) + self.similarity(self.warped_image_B, image_A)
+
+        if len(self.input_shape) - 2 == 3:
+            Iepsilon = (
+                self.identity_map
+                + 2 * torch.randn(*self.identity_map.shape).to(config.device)
+                / self.identity_map.shape[-1]
+            )[:, :, ::2, ::2, ::2]
+        elif len(self.input_shape) - 2 == 2:
+            Iepsilon = (
+                self.identity_map
+                + 2 * torch.randn(*self.identity_map.shape).to(config.device)
+                / self.identity_map.shape[-1]
+            )[:, :, ::2, ::2]
+
+        # compute squared Frobenius of Jacobian of icon error
+
+        direction_losses = []
+
+        approximate_Iepsilon = self.phi_AB(self.phi_BA(Iepsilon))
+
+        inverse_consistency_error = Iepsilon - approximate_Iepsilon
+
+        delta = 0.001
+
+        if len(self.identity_map.shape) == 4:
+            dx = torch.Tensor([[[[delta]], [[0.0]]]]).to(config.device)
+            dy = torch.Tensor([[[[0.0]], [[delta]]]]).to(config.device)
+            direction_vectors = (dx, dy)
+
+        elif len(self.identity_map.shape) == 5:
+            dx = torch.Tensor([[[[[delta]]], [[[0.0]]], [[[0.0]]]]]).to(config.device)
+            dy = torch.Tensor([[[[[0.0]]], [[[delta]]], [[[0.0]]]]]).to(config.device)
+            dz = torch.Tensor([[[[0.0]]], [[[0.0]]], [[[delta]]]]).to(config.device)
+            direction_vectors = (dx, dy, dz)
+        elif len(self.identity_map.shape) == 3:
+            dx = torch.Tensor([[[delta]]]).to(config.device)
+            direction_vectors = (dx,)
+
+        for d in direction_vectors:
+            approximate_Iepsilon_d = self.phi_AB(self.phi_BA(Iepsilon + d))
+            inverse_consistency_error_d = Iepsilon + d - approximate_Iepsilon_d
+            grad_d_icon_error = (
+                inverse_consistency_error - inverse_consistency_error_d
+            ) / delta
+            direction_losses.append(torch.mean(grad_d_icon_error**2))
+
+        inverse_consistency_loss = sum(direction_losses)
+
+        all_loss = self.lmbda * inverse_consistency_loss + similarity_loss
+
+        transform_magnitude = torch.mean(
+            (self.identity_map - self.phi_AB_vectorfield) ** 2
+        )
+        return icon.losses.ICONLoss(
+            all_loss,
+            inverse_consistency_loss,
+            similarity_loss,
+            transform_magnitude,
+            icon.losses.flips(self.phi_BA_vectorfield),
+        )
+    
 BendingLoss = namedtuple(
     "BendingLoss",
     "all_loss bending_energy_loss similarity_loss transform_magnitude flips",
